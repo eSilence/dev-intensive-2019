2c3c75ae73634ac0784bc778a07a3451102040c3
eSilence <eSilence@yandex.ru>
2019-07-02 20:09:19 +0300

    +(2) add Base Message, AbstractFactory

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -1,4 +1,53 @@
 package ru.skillbranch.devintensive.models
 
-class User {
+import ru.skillbranch.devintensive.utils.Utils
+import java.util.*
+
+/*  1 задание
+DTO User, Factory
+Необходимо создать data класс User и реализовать Factory для создания экземпляров класса
+Создай data class User содержащий сделующие свойства:
+val id : String,
+var firstName : String?,
+var lastName : String?,
+var avatar : String?,
+var rating : Int = 0,
+var respect : Int = 0,
+var lastVisit : Date? = Date(),
+var isOnline : Boolean = false
+
+Реализуй паттерн Factory с методом makeUser(fullName) принимающий в качесте аргумента полное имя пользователя и возвращающий экземпляр класса User
+ */
+data class User (
+    val id : String,
+    var firstName : String?,
+    var lastName : String?,
+    var avatar : String?,
+    var rating : Int = 0,
+    var respect : Int = 0,
+    var lastVisit : Date? = Date(),
+    var isOnline : Boolean = false){
+
+    constructor(id:String, firstName:String?, lastName:String?) :
+            this(id = id, firstName = firstName, lastName = lastName, avatar = null)
+
+    constructor(id:String) :
+            this(id, "John", "Doe $id")
+
+    init{
+        println("It's alive!! $id '$firstName' '$lastName'" )
+    }
+
+    companion object Factory { //перед созданием объекта необходимо сделать какие-либо преоразования
+        private var lastId:Int = -1
+
+        fun makeUser(fullName:String?): User{
+            lastId++
+
+            return User("$lastId",
+                Utils.parseFullName(fullName).first,
+                Utils.parseFullName(fullName).second)
+        }
+    }
+
 }
--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -1,4 +1,19 @@
 package ru.skillbranch.devintensive.utils
+/* 3 задание
+Реализуй метод Utils.parseFullName(fullName) принимающий в качестве аргумента полное имя пользователя (null, пустую строку) и возвращающий пару значений Pair(firstName, lastName) при невозможности распарсить полное имя или его часть вернуть null null/"firstName" null
+Пример:
+Utils.parseFullName(null) //null null
+Utils.parseFullName("") //null null
+Utils.parseFullName(" ") //null null
+Utils.parseFullName("John") //John null
+*/
 
 object Utils {
+    fun parseFullName(fullName:String?):Pair<String?, String?> {
+        val parts: List<String>? = fullName?.split(" ")
+        val firstName = parts?.getOrNull(0)
+        val lastName = parts?.getOrNull(1)
+        val result: Pair<String?, String?> = Pair((if(firstName == "")null else firstName), (if(lastName == "")null else lastName))
+        return result
+    }
 }
