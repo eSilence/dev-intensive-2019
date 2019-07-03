4fbff83c3be0883a22fca50194270fe3048830ae
eSilence <eSilence@yandex.ru>
2019-07-03 12:19:59 +0300

    +(7) add Utils.transliteration

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -1,5 +1,8 @@
 package ru.skillbranch.devintensive.utils
-/* 3 задание
+
+
+object Utils {
+    /* 3 задание
 Реализуй метод Utils.parseFullName(fullName) принимающий в качестве аргумента полное имя пользователя (null, пустую строку) и возвращающий пару значений Pair(firstName, lastName) при невозможности распарсить полное имя или его часть вернуть null null/"firstName" null
 Пример:
 Utils.parseFullName(null) //null null
@@ -7,8 +10,6 @@ Utils.parseFullName("") //null null
 Utils.parseFullName(" ") //null null
 Utils.parseFullName("John") //John null
 */
-
-object Utils {
     fun parseFullName(fullName: String?): Pair<String?, String?> {
         val parts: List<String>? = fullName?.split(" ")
         val firstName = parts?.getOrNull(0)
@@ -52,5 +53,31 @@ Utils.toInitials(" ", "") //null
             if (part1 != null && part2 != null) part1 + part2 else if (part1 == null && part2 == null) null else if (part1 == null) part2 else part1
         return result
     }
-}
+
+    /*
+    *transliteration
+Необходимо реализовать утилитный метод transliteration(payload divider) принимающий в качестве аргумента строку
+и возвращающий преобразованную строку из латинских символов
+Реализуй метод Utils.transliteration(payload divider) принимающий в качестве аргумента строку (divider по умолчанию " ")
+и возвращающий преобразованную строку из латинских символов, словарь символов соответствия алфовитов доступен в ресурсах к заданию
+Пример:
+Utils.transliteration("Женя Стереотипов") //Zhenya Stereotipov
+Utils.transliteration("Amazing Петр","_") //Amazing_Petr
+     */
+
+    fun transliteration(str:String, divider:String = " "):String{
+    //   val readWriteMap = hashMapOf(" " to "$divider", "а" to "a", "б" to "b", "в" to "v", "г" to "g", "д" to "d", "е" to "e", "ё" to "e", "ж" to "zh", "з" to "z", "и" to "i", "й" to "i", "к" to "k", "л" to "l", "м" to "m", "н" to "n", "о" to "o", "п" to "p", "р" to "r", "с" to "s", "т" to "t", "у" to "u", "ф" to "f", "х" to "h", "ц" to "c", "ч" to "ch", "ш" to "sh", "щ" to "sh'", "ъ" to "", "ы" to "i", "ь" to "", "э" to "e", "ю" to "yu", "я" to "ya")
+       val replaceMap = hashMapOf(' ' to "$divider", 'а' to "a", 'б' to "b", 'в' to "v", 'г' to "g", 'д' to "d", 'е' to "e", 'ё' to "e", 'ж' to "zh", 'з' to "z", 'и' to "i", 'й' to "i", 'к' to "k", 'л' to "l", 'м' to "m", 'н' to "n", 'о' to "o", 'п' to "p", 'р' to "r", 'с' to "s", 'т' to "t", 'у' to "u", 'ф' to "f", 'х' to "h", 'ц' to "c", 'ч' to "ch", 'ш' to "sh", 'щ' to "sh'", 'ъ' to "", 'ы' to "i", 'ь' to "", 'э' to "e", 'ю' to "yu", 'я' to "ya")
+       val builder = StringBuilder()
+       for (letter in str)
+           when (letter) {
+               in 'а' .. 'я' -> builder.append(replaceMap[letter])
+               in 'А' .. 'Я' -> builder.append((replaceMap[letter.toLowerCase()])?.toUpperCase())
+               ' ' -> builder.append(replaceMap[letter])
+               else -> builder.append(letter)
+           }
+       return builder.toString()
+    }
+
+    }
 
