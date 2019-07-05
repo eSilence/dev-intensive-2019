5db0174d59edfab3262aa32196d2c67f4185acc4
eSilence <eSilence@yandex.ru>
2019-07-05 16:23:20 +0300

    (+12) add String.stripHtml edit String.truncate

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -11,11 +11,26 @@ package ru.skillbranch.devintensive.extensions
 "Bender Bending Rodriguez — дословно «Сгибальщик Сгибающий Родригес»".truncate(15) //Bender Bending...
 "A     ".truncate(3) //A
  */
-fun String.truncate(count:Int = 16 ):String{
-    var str = this.trim()//this.take(count)
-    if (str.length <= count){
-        return str
-    }
-        str = str.take(count)
-        return str.trim()+"..."
+fun String.truncate(count:Int = 16 ) =
+    if (count >= this.trim().length)
+        this.trim()
+    else
+        "${this.trim().take(count).trim()}..."
+
+/*
+*String.stripHtml
+Необходимо реализовать метод stripHtml для очистки строки от лишних пробелов, html тегов, escape последовательностей
+Реализуй extension позволяющий очистить строку от html тегов
+и html escape последовательностей ("& < > ' ""),
+а так же удалить пустые символы (пробелы) между словами если их больше 1.
+Необходимо вернуть модифицированную строку
+Пример:
+"<p class="title">Образовательное IT-сообщество Skill Branch</p>".stripHtml() //Образовательное IT-сообщество Skill Branch
+"<p>Образовательное       IT-сообщество Skill Branch</p>".stripHtml() //Образовательное IT-сообщество Skill Branch
+ */
+fun String.stripHtml(): String {
+    val htmlRegex = Regex("(<.*?>)|(&[^ а-я]{1,4}?;)")
+    val spaceRegex = Regex(" {2,}")
+    return this.replace(htmlRegex, "").replace(spaceRegex, " ")
+ //  return this.replace(Regex("(<.*?>)|(&.*?;)|([ ]{2,})"), " ").trim()
 }
